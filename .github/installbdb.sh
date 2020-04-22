da40ba7817115a403c98aefec318daf2be892ee0
Aleš Kalan <SloRunner@users.noreply.github.com>
2020-04-22 04:46:45 +0200

    Update installbdb.sh

--- .github/installbdb.sh
+++ .github/installbdb.sh
@@ -1,11 +1,6 @@
-sudo ./autogen.sh
-sudo mkdir db4
-sudo wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
-sudo tar -xzvf db-4.8.30.NC.tar.gz
-cd db-4.8.30.NC/build_unix/
-sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
-sudo make install
-cd ../../
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 sudo ./autogen.sh
 sudo ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/"
-sudo make -j2
+sudo make -j$(nproc)
