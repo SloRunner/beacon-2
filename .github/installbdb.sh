2ed5fd3b76bbb0c82f10e725543c83a8ab985709
Aleš Kalan <SloRunner@users.noreply.github.com>
2020-04-22 04:46:57 +0200

    Update installbdb.sh

--- .github/installbdb.sh
+++ .github/installbdb.sh
@@ -2,5 +2,5 @@ sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
 sudo ./autogen.sh
-sudo ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/"
+sudo ./configure
 sudo make -j$(nproc)
