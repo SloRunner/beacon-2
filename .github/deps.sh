bf5eefcf06601b64d497480d97288c7ebb49a5ec
Aleš Kalan <SloRunner@users.noreply.github.com>
2020-04-22 06:09:13 +0200

    Create deps.sh

---
+++ .github/deps.sh
@@ -0,0 +1,13 @@
+sudo apt-get install libevent-dev libboost-all-dev -y
+sudo apt install build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git -y
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y
+
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
+sudo apt-get install libzmq3-dev -y
+sudo apt-get install libminiupnpc-dev -y
+sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev openssl -y
+
+sudo apt install g++-mingw-w64-x86-64 -y
+echo 1 | sudo update-alternatives --config x86_64-w64-mingw32-g++
