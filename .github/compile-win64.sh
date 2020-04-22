5fea549cb054ed56e3c76d56ec9012eeed034bb5
Aleš Kalan <SloRunner@users.noreply.github.com>
2020-04-22 05:33:51 +0200

    Update compile-win64.sh

--- .github/compile-win64.sh
+++ .github/compile-win64.sh
@@ -6,14 +6,5 @@ PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g')
 cd `pwd`/depends
 sudo make -j2 HOST=x86_64-w64-mingw32
 cd ..
-sudo ./autogen.sh
-sudo mkdir db4
-sudo wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
-sudo tar -xzvf db-4.8.30.NC.tar.gz
-cd db-4.8.30.NC/build_unix/
-sudo ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
-sudo make install
-cd ../../
-sudo ./autogen.sh
-sudo ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-w64-mingw32
+sudo ./configure --prefix=`pwd`/depends/x86_64-w64-mingw32
 sudo make -j2
