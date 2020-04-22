414078a2d503708c32066a0891b6a1b570238c81
Aleš Kalan <SloRunner@users.noreply.github.com>
2020-04-22 06:11:26 +0200

    Update compile-win64.sh

--- .github/compile-win64.sh
+++ .github/compile-win64.sh
@@ -3,6 +3,3 @@ PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g')
 cd `pwd`/depends
 sudo make -j2 HOST=x86_64-w64-mingw32
 cd ..
-sudo ./autogen.sh
-sudo ./configure --prefix=`pwd`/depends/x86_64-w64-mingw32
-sudo make -j2
